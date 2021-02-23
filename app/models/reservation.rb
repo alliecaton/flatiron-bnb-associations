d31221fc3dc08904fea8aec9c4bfd5f1d9fc9b7a
Allie <alliecaton3@gmail.com>
2021-02-23 11:31:53 -0500

    commit tree f67a38097cecd17863ce3db2e18dbf8f2f2c9600

---
+++ app/models/reservation.rb
@@ -0,0 +1,8 @@
+class Reservation < ApplicationRecord
+    belongs_to :listing 
+    belongs_to :guest, class_name: "User"
+    belongs_to :host, class_name: "User"
+    has_many :reviews
+    
+
+end
